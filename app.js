b5b612d732abb7d721c4c3077b40893d0631e05f
probingia <164015237+probingia@users.noreply.github.com>
2025-09-18 23:22:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -723,7 +723,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ===================================================================================
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('service-worker.js')
                 .then(registration => {
                     console.log('Service Worker registrado con éxito:', registration.scope);
                 })
